3cd669f6e6aed3a39627ee090b6d35a5bc1d9fb9
akajhon <joaopedrorosa03@gmail.com>
2023-11-18 14:41:46 -0300

    fix: start script

--- projeto-web/start.sh
+++ projeto-web/start.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-FILE_NAME="GoogleNews-vectors-negative300.bin.zip"
+ZIP_FILE="GoogleNews-vectors-negative300.bin.zip"
+EXTRACTED_FILE="GoogleNews-vectors-negative300.bin"
 
-echo "[+] Descomprimindo o arquivo..."
-unzip "$FILE_NAME"
+if [ ! -f "$EXTRACTED_FILE" ]; then
+    echo "[+] Arquivo $EXTRACTED_FILE não encontrado. Descomprimindo o arquivo..."
+    unzip "$ZIP_FILE"
+else
+    echo "[+] Arquivo $EXTRACTED_FILE já foi extraído."
+fi
 
 echo "[+] Instalando Dependências..."
 pip install -r requirements.txt
