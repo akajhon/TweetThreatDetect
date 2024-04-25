64cdbeba46315612e709da4345803b1fdb60fc9d
João Pedro <69048604+akajhon@users.noreply.github.com>
2024-04-25 20:26:09 -0300

    Updated Dockerfile and start.sh script

--- start.sh
+++ start.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
 
 # Variables
-FILEID="1QZ9OIr0vWYJDYThMBaY76UJcqWwIjHFP"
-ZIP_FILE="GoogleNews-vectors-negative300.zip"
+ZIP_FILE="GoogleNews-vectors-negative300.bin.gz"
 EXTRACTED_FILE="./models/GoogleNews-vectors-negative300.bin"
-PATH_IN_ZIP="GoogleNews-vectors-negative300.bin"
-CONFIRM_TOKEN=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate "https://docs.google.com/uc?export=download&id=$FILEID" -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')
 
 if [ ! -f "$EXTRACTED_FILE" ]; then
-    echo "[+] Arquivo $EXTRACTED_FILE não encontrado.  Baixando e Descomprimindo o arquivo..."
-    wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$CONFIRM_TOKEN&id=$FILEID" -O $ZIP_FILE && rm -rf /tmp/cookies.txt
-    unzip -j "$ZIP_FILE" "$PATH_IN_ZIP" -d "./models"
+    echo "[+] Arquivo $EXTRACTED_FILE não encontrado. Baixando e descomprimindo o arquivo..."
+    wget "https://figshare.com/ndownloader/files/41403483" -O $ZIP_FILE
+    gunzip -c $ZIP_FILE > $EXTRACTED_FILE
 else
     echo "[+] Arquivo $EXTRACTED_FILE já foi extraído."
 fi
 
-echo "[+] Removendo arquivo .zip..."
-rm -rf ./GoogleNews-vectors-negative300.zip
+echo "[+] Removendo arquivo .gz..."
+rm -rf ./GoogleNews-vectors-negative300.bin.gz
 
 echo "[+] Instalando Dependências..."
 pip install -r requirements.txt
